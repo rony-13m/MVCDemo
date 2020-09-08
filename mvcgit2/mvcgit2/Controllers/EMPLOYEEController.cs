48946dc5250b503572bef98357db617eefa55e37
reenyjoseph12@gmail.com <reenyjoseph12@gmail.com>
2020-09-08 15:10:46 +0530

    employyecontroller

---
+++ mvcgit2/mvcgit2/Controllers/EMPLOYEEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcgit2.Controllers
+{
+    public class EMPLOYEEController : Controller
+    {
+        // GET: EMPLOYEE
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
